6e4129a9d62fc3aa3a13835eb76491bed73653ba
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-15 23:50:44 +0000

    Wed Nov 15 23:50:44 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -10,6 +10,7 @@ public class FizzBuzz {
     public static final String BUZZ = "buzz";
     public static final String FIZZ = "fizz";
     public static final String DELUXE = "deluxe";
+    public static final String FAKE = "fake";
     public static final String THREE = "3";
     public static final String FIVE = "5";
 
@@ -35,6 +36,9 @@ public class FizzBuzz {
             }
         }
         if(isDeluxe){
+            if(number%2 != 0){
+                builder.append(" ").append(FAKE);
+            }
             builder.append(" ").append(DELUXE);
         }
         if(builder.length() == 0){

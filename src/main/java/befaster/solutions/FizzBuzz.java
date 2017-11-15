02aabf875d0a894e533c7c7bcb5aea02b7e03a70
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-15 23:43:44 +0000

    Wed Nov 15 23:43:44 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -9,7 +9,7 @@ public class FizzBuzz {
     public static final String FIZZ_BUZZ = "fizz buzz";
     public static final String BUZZ = "buzz";
     public static final String FIZZ = "fizz";
-    public static final String DELUXE = "Deluxe";
+    public static final String DELUXE = "deluxe";
     public static final String THREE = "3";
     public static final String FIVE = "5";
 

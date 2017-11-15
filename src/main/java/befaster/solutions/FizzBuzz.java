ebe50549aa18e141f15d63207e22d8473499cbfa
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-15 23:26:01 +0000

    Wed Nov 15 23:26:01 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -2,16 +2,21 @@ package befaster.solutions;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.Arrays;
+
 public class FizzBuzz {
 
     public static final String FIZZ_BUZZ = "fizz buzz";
     public static final String BUZZ = "buzz";
     public static final String FIZZ = "fizz";
+    public static final String DELUXE = "Deluxe";
     public static final String THREE = "3";
     public static final String FIVE = "5";
 
     public static String fizzBuzz(Integer number) {
         String numberStr = number.toString();
+        boolean isDeluxe = number > 10 && numberStr.replaceAll(numberStr.substring(0,1),"").equals("");
+        System.out.println(isDeluxe);
         if(number%15 == 0){
             return FIZZ_BUZZ;
         } else if(number%5 == 0){
@@ -25,6 +30,13 @@ public class FizzBuzz {
         }else {
             return number.toString();
         }
+    }
+
+    public static void main(String[] args) {
+        fizzBuzz(1);
+        fizzBuzz(101);
+        fizzBuzz(11);
+        fizzBuzz(9999999);
     }
 
 }
